c4e106ffa3dfcd1a3ec6079e2f8602d729de0452
Andrew Bray <a.bray@cardano.com>
2020-07-24 12:54:16 +0100

    Pitching section1

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# Step 1
+from data import games
+plays = games[games['type'] == 'play']
+
+# Step 2
+strike_outs = plays[plays['event'].str.contains('K')]
+
+# Step 3
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+
+# Step 4
+strike_outs = strike_outs.reset_index(name='strike_outs')
+
+# Step 5
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+# Step 6
+strike_outs.plot(x='year', y='strike_outs', type='scatter').legend('Strike Outs')
+plt.show()
